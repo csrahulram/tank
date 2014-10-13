2766306c62d570492ea7b06c638967ed87772479
Rahul <csrahulram@gmail.com>
2014-10-13 23:28:21 +0530

    Socket io added.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	
+	createTank();
   res.render('index', { title: "tank"});
 });
+
+
+function createTank() {
+
+}
 
 module.exports = router;
